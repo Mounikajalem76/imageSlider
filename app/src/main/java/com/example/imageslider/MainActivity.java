ab194b18549505f64453db4bcd9609efe4aab932
Durga <durgaprasadsetti67@gmail.com>
2024-07-25 20:28:45 +0530

    imageSlider

---
+++ app/src/main/java/com/example/imageslider/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.imageslider;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.denzcoskun.imageslider.ImageSlider;
+import com.denzcoskun.imageslider.constants.ScaleTypes;
+import com.denzcoskun.imageslider.models.SlideModel;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        ImageSlider imageSlider=(ImageSlider) findViewById(R.id.imageslider);
+
+        ArrayList<SlideModel> slideModels=new ArrayList<>();
+
+        slideModels.add(new SlideModel(R.drawable.image1, ScaleTypes.FIT));
+        slideModels.add(new SlideModel(R.drawable.image2, ScaleTypes.FIT));
+        slideModels.add(new SlideModel(R.drawable.image3, ScaleTypes.FIT));
+        slideModels.add(new SlideModel(R.drawable.image4, ScaleTypes.FIT));
+        slideModels.add(new SlideModel(R.drawable.image5, ScaleTypes.FIT));
+
+
+        imageSlider.setImageList(slideModels,ScaleTypes.FIT);
+    }
+}
